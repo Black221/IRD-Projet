217818656f05394963b95fe605a0e077405cf399
Cisco-dev <78382270+Cisco-dev@users.noreply.github.com>
2022-06-12 19:16:13 +0000

    Add files via upload

---
+++ API-Ressource/controllers/ecg.controller.js
@@ -0,0 +1,111 @@
+const EcgModel = require('../models/EcgModel');
+const MetadataModel = require('../models/MetadataModel');
+const validId = require('mongoose').Types.ObjectId;
+
+/**
+ * @description - This controller is used to list all ecg .
+ */
+module.exports.getAllEcg = async(req, res) => {
+        try {
+            const ecgData = await EcgModel.find();
+            res.status(200).json(ecgData);
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+        }
+    }
+    /**
+     * @description - This controller is used to get ecg by id.
+     * @param {string} id - The id of ecg.
+     * chekc if id is valid before doing anything else.
+     */
+module.exports.getEcgById = async(req, res) => {
+        if (!validId.isValid(req.params.id))
+            return res.status(400).json({
+                message: 'Invalid id'
+            });
+
+        try {
+            const ecgData = await EcgModel.findById({
+                _id: req.params.id
+            });
+            res.status(200).json(ecgData);
+
+        } catch (error) {
+            res.status(500).json({
+                message: error
+            });
+        }
+    }
+    /**
+     * @description - This controller permits us to post or create new ecg.
+     */
+module.exports.postOneEcg = async(req, res) => {
+        try {
+            const metadataId = await new MetadataModel({
+                created_by: req.body.created_by,
+                last_updated_by: req.body.last_updated_by,
+            });
+            const metadataIdSave = await metadataId.save();
+
+            const ecgId = await new EcgModel({
+                metadata_id: metadataIdSave.id,
+                nameDataset: req.body.nameDataset,
+                filename: req.body.filename,
+                filepath: req.body.filepath,
+                state: req.body.state
+            });
+            const ecgIdSave = await ecgId.save();
+            res.status(200).json(ecgIdSave);
+
+        } catch (error) {
+            res.status(500).json({
+                message: error
+            });
+        }
+
+    }
+    /**
+     * @description - This controller permits us to update ecg.
+     * @param {string} id - The id of ecg.
+     */
+module.exports.updateEcg = async(req, res) => {
+        if (!validId.isValid(req.params.id))
+            return res.status(400).json({
+                message: 'Invalid id'
+            });
+
+        try {
+            const ecgData = await EcgModel.findByIdAndUpdate({
+                _id: req.params.id
+            }, req.body, {
+                new: true
+            });
+        } catch (err) {
+            res.status(500).json({
+                message: err
+            });
+        }
+    }
+    /**
+     * @description - This controller permits us to delete ecg.
+     * @param {string} id - The id of ecg.
+     */
+
+module.exports.deleteEcg = async(req, res) => {
+    if (!validId.isValid(req.params.id))
+        return res.status(400).json({
+            message: 'Invalid id'
+        });
+
+    try {
+        const ecgData = await EcgModel.findByIdAndDelete({
+            _id: req.params.id
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err
+        });
+    }
+}
