8128198aab65e9c25d0af6c1ed2910638a990844
MarieWalyFall <107265901+MarieWalyFall@users.noreply.github.com>
2022-06-15 15:10:30 +0000

    Add files via upload

--- API-Ressource/db.js
+++ API-Ressource/db.js
@@ -1,6 +1,7 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
+const fs = require('fs')
 
 // const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASS+"@guapicluster.z5rby.mongodb.net/"+process.env.DB_NAME;
 const uri = "mongodb+srv://ird_esp:passer@dbtest.mlfjm.mongodb.net/?retryWrites=true&w=majority"
@@ -16,6 +17,10 @@ let localConnect = function () {
     db.on('error', console.error.bind(console, 'Erreur lors de la de connection à la base de donnée...'))
     db.once('open', function(){
         console.log('Connexion à la base de donnée établie...: http://localhost:'+process.env.PORT);
+        var dir= './ECG'
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir)
+        }
     })
 }
 
@@ -25,6 +30,10 @@ client.connect(err => {
     if (!err) {
         connected = true;
         console.log("Connected to MongoDB");
+        var dir= './ECG'
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir)
+        }
     } else {
         console.log("Error to connect on MongoDB: ", err);
         console.log("Trying to connect on local db ....");
--- API-Ressource/server.js
+++ API-Ressource/server.js
@@ -4,20 +4,24 @@
 require('dotenv').config({ path: './.env' });
 require('./db')
 const express = require('express');
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const routeForEcg = require('./routes/route.ecg');
 const routeForPatient = require('./routes/route.patient');
 const routeForMedicalStaff = require('./routes/route.medicalStaff');
-const routeForDataset = require('./routes/dataset.route');
+const routeForDataset = require('./routes/route.dataset');
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const fs = require('fs')
+const fileUpload = require('express-fileupload');
+const replace = require('replace');
 
 /**
  * middlewares
  */
 app.use(bodyParser.json());
-app.use(cors('http://localhost:4200'));
+
+app.use(fileUpload({createParentPath: true}))
 app.use('/ecg', routeForEcg);
 app.use('/patient', routeForPatient);
 app.use('/medicalStaff', routeForMedicalStaff);
