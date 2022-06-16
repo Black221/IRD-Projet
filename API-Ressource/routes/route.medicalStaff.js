10b377d162067408f3bbae833fd915041cd08397
MarieWalyFall <107265901+MarieWalyFall@users.noreply.github.com>
2022-06-16 10:34:57 +0000

    Add files via upload

--- API-Ressource/routes/route.medicalStaff.js
+++ API-Ressource/routes/route.medicalStaff.js
@@ -4,12 +4,12 @@ const medicalStaffController = require('../controllers/medicalStaff.controller')
 
 /**
  * routes for medicalStaff
- */
+**/
 
-routerMedicalStaff.get('/', medicalStaffController.getAllMedicalStaff);
-routerMedicalStaff.get('/:id', medicalStaffController.getMedicalStaffById);
-routerMedicalStaff.post('/', medicalStaffController.postOneMedicalStaff);
-routerMedicalStaff.put('/:id', medicalStaffController.updateMedicalStaff);
-routerMedicalStaff.delete('/:id', medicalStaffController.deleteMedicalStaff);
+routerMedicalStaff.get('/getAll/:userId', medicalStaffController.getAllMedicalStaff);
+routerMedicalStaff.get('/getOne/:medicalStaffId', medicalStaffController.getMedicalStaffById);
+routerMedicalStaff.post('/postOne/:posterId', medicalStaffController.postOneMedicalStaff); 
+routerMedicalStaff.put('/updateOne/updaterId/:medicalStaffId', medicalStaffController.updateMedicalStaff);
+routerMedicalStaff.delete('/deleteOne/deleterId/:medicalStaffId', medicalStaffController.deleteMedicalStaff);
 
 module.exports = routerMedicalStaff;
