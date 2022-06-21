a910e4d83dda1741a7046cbc7c66208051b07b74
Cisco-dev <78382270+Cisco-dev@users.noreply.github.com>
2022-06-21 12:59:33 -0400

    Add files via upload

--- API-Ressource/db.js
+++ API-Ressource/db.js
@@ -1,44 +1,53 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 const fs = require('fs')
 
-// const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASS+"@guapicluster.z5rby.mongodb.net/"+process.env.DB_NAME;
-const uri = "mongodb+srv://ird_esp:passer@dbtest.mlfjm.mongodb.net/?retryWrites=true&w=majority"
+// // const uri = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASS+"@guapicluster.z5rby.mongodb.net/"+process.env.DB_NAME;
+// const uri = "mongodb+srv://ird_esp:passer@dbtest.mlfjm.mongodb.net/?retryWrites=true&w=majority"
 
-const uri_local = "mongodb://localhost:27017/"+process.env.DB_LOCAL_NAME;
-let connected = false;
-let start = false;
+// const uri_local = "mongodb://localhost:27017/" + process.env.DB_LOCAL_NAME;
+// let connected = false;
+// let start = false;
 
-let localConnect = function () {
-    // DB connection
-    mongoose.connect(uri_local)
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'Erreur lors de la de connection à la base de donnée...'))
-    db.once('open', function(){
-        console.log('Connexion à la base de donnée établie...: http://localhost:'+process.env.PORT);
-        var dir= './ECG'
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir)
-        }
-    })
-}
+// let localConnect = function() {
+//     // DB connection
+//     mongoose.connect(uri_local)
+//     const db = mongoose.connection;
+//     db.on('error', console.error.bind(console, 'Erreur lors de la de connection à la base de donnée...'))
+//     db.once('open', function() {
+//         console.log('Connexion à la base de donnée établie...: http://localhost:' + process.env.PORT);
+//         var dir = './ECG'
+//         if (!fs.existsSync(dir)) {
+//             fs.mkdirSync(dir)
+//         }
+//     })
+// }
 
 
-//const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
-const client = new MongoClient(uri_local, { useNewUrlParser: true, useUnifiedTopology: true});
-client.connect(err => {
-    if (!err) {
-        connected = true;
-        console.log("Connected to MongoDB");
-        var dir= './ECG'
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir)
-        }
-    } else {
-        console.log("Error to connect on MongoDB: ", err);
-        console.log("Trying to connect on local db ....");
-        localConnect();
+// //const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+// const client = new MongoClient(uri_local, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//     if (!err) {
+//         connected = true;
+//         var dir = './ECG'
+//         if (!fs.existsSync(dir)) {
+//             fs.mkdirSync(dir)
+//         }
+//         console.log("Connected to MongoDB");
+//     } else {
+//         console.log("Error to connect on MongoDB: ", err);
+//         console.log("Trying to connect on local db ....");
+//         // localConnect();
+//     }
+//     // perform actions on the collection object
+// });
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/' + process.env.DB_LOCAL_NAME);
+    console.log("Connected to MongoDB");
+    var dir = './ECG'
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir)
     }
-    // perform actions on the collection object
-});
+}
