7aed397f7950b3f022a44cdf73534298f84f5d70
Cisco-dev <78382270+Cisco-dev@users.noreply.github.com>
2022-06-27 15:46:05 -0400

    Add files via upload

--- API-Ressource/config/db.config.js
+++ API-Ressource/config/db.config.js
@@ -1,17 +1,8 @@
+const { mongoose } = require('mongoose')
 
-/**
- * Connexion to our database
- */
-
-//mongodb+srv://ird_esp:passer@dbtest.mlfjm.mongodb.net/?retryWrites=true&w=majority
-
-import mongoose from "mongoose";
-
-main().catch(err => console.log(err));
+main().catch(err => console.log(err))
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/apiRessources');
-    console.log('Connected to MongoDB database successfully');
+    await mongoose.connect('mongodb://localhost:27017/' + process.env.DB_LOCAL_NAME)
+    console.log("connexion etablie avec " + process.env.DB_LOCAL_NAME + " avec succes !")
 }
-
-module.exports = main;
