647fa67c627a938284beb05c318c73bb0fae6c4f
LHackSRT <78321063+Black221@users.noreply.github.com>
2022-06-28 22:39:20 +0000

    Delete Platfrom directory

--- Platfrom/src/models/activity.model.ts
+++
@@ -1,9 +0,0 @@
-
-export class ActivityModel {
-
-    constructor(public name: string,
-                public last_update_at: Date,
-                public last_view_at : Date,
-                public objectName : Date
-               ) {}
-}
